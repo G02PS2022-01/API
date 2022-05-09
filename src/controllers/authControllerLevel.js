29906ea177415a8d4c68e3c404ff136b07148d24
marcos1079 <marcos.antonio2306@outlook.com>
2022-05-09 12:53:41 -0300

    feature/I03/R34/authController-Level autenticação collection Level

---
+++ src/controllers/authControllerLevel.js
@@ -0,0 +1,23 @@
+const express = require("express");
+
+const Level = require("../models/Level");
+
+const router = express.Router();
+
+router.post("/level", async (req, res) => {
+  try {
+    if (await Level.findOne({ level }))
+    return res.status(400).send({ erro:'User already exists' });
+
+
+    const level = await Level.create(req.body);
+
+    return res.send({ level });
+  } catch (err) {
+    return res.status(400).send({ error: "Registrain failed" });
+  }
+});
+
+
+
+module.exports = (app) => app.use("/auth", router);
