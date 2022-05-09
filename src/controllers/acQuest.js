e27b5bf32d65efd3a614574638e2ca7be3d57bc9
nicolasarthur10 <nicolasarthur.26@hotmail.com>
2022-05-09 10:01:31 -0300

    autenticacao da quest

---
+++ src/controllers/acQuest.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+const Quest = require("../models/Quest");
+
+const router = express.Router();
+
+router.post("/register", async (req, res) => {
+  try {
+    const quest = await Quest.create(req.body);
+
+    return res.send({ quest });
+  } catch (err) {
+    return res.status(400).send({ error: "Registrain failed" });
+  }
+});
+
+module.exports = (app) => app.use("/auth", router);
