6f87f1e12a59412c7a553d10061dfd6a0fb32fe5
a1 <54730696+amaur1mmj@users.noreply.github.com>
2022-05-09 08:31:47 -0300

    Delete index.js
    Upado de maneira indevida

--- src/database/index.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-mongoose.connect(
-  "mongodb+srv://amauri_1:adm12345@dogcode.aqwiq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
-);
-
-mongoose.Promise = global.Promise;
-
-module.exports = mongoose;
