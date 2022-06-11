e282cea84bda29944a44f869a7f396aebc7c91a1
amaur1mmj@gmail.com <amaur1mmj@gmail.com>
2022-06-11 09:31:23 -0300

    Atualizações necessárias tudo funcionando.

--- src/database/index.js
+++ src/database/index.js
@@ -1,15 +1,16 @@
 const mongoose = require("mongoose");
 const connectDB = require("../config/connectDB");
+//const uriCFG = require("../config/uriCFG");
 
 //colocar isso em lugar seguro
-const username = encodeURIComponent(connectDB.username);
+const username = encodeURIComponent(connectDB.usr);
 const password = encodeURIComponent(connectDB.password);
-const cluster = connectDB.cluster;
+const cluster = connectDB.clst;
 //const authCluster = "Dog-test?retryWrites=true";
 //const autMechanism = "w=majority";
 
-//const MONGODB_connect = `mongodb+srv://${username}:${password}@${cluster}/DogeBD?retryWrites=true&w=majority`;
-const MONGODB_connect = `mongodb+srv://amauri_1:adminok@dogcode.aqwiq.mongodb.net/DogeBD?retryWrites=true&w=majority`;
+const MONGODB_connect = `mongodb+srv://${username}:${password}@${cluster}/DogeBD?retryWrites=true&w=majority`;
+//const MONGODB_connect = `mongodb+srv://admLocal:adminok@dogcode.aqwiq.mongodb.net/DogeBD?retryWrites=true&w=majority`;
 
 mongoose.connect(MONGODB_connect);
 
